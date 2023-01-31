b0f7bd5765aa223213e94a02332f1ba47e58eb4b
DEVFATH <ggje33@gmail.com>
2023-01-31 17:46:04 +0900

    [설정] eslint 설정 수정

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,27 +5,34 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+    createDefaultProgram: true,
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*', // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
+    'object-curly-spacing': ['error', 'always'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'const', next: 'return' },
+      { blankLine: 'always', prev: 'let', next: 'return' },
+    ],
+    'space-in-parens': ['error', 'never'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'import/no-unresolved': 0,
+    semi: ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
   },
 };
