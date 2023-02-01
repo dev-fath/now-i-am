bbfcd104c21720af259629807cc5a6d8f0010689
DEVFATH <ggje33@gmail.com>
2023-02-01 15:52:32 +0900

    [설정] firebase key env파일로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -28,19 +28,20 @@ import './theme/variables.scss';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCMGHHjn3k4lSBW20O6-6FVWG0wFCZEXUw',
-  authDomain: 'now-i-am.firebaseapp.com',
-  projectId: 'now-i-am',
-  storageBucket: 'now-i-am.appspot.com',
-  messagingSenderId: '459022669812',
-  appId: '1:459022669812:web:a109a34c61bf5d9d4c0ba1',
-  measurementId: 'G-PG6SBNKVJS',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
