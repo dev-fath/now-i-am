2879bb9c282a5905b17018e9945b153cdd4277ab
DEVFATH <ggje33@gmail.com>
2023-02-02 18:09:05 +0900

    [글쓰기] 작성 취소버튼 추가

--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -1,11 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import BackButton from 'components/BackButton';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+import './Write.scss';
+import { useHistory } from 'react-router-dom';
 
 const Write = () => {
+  const history = useHistory();
+  const onClickCancel = () => {
+    const result = window.confirm(
+      '페이지에서 벗어나면 작성된 내용이 모두 지워집니다.\n 그래도 나가시겠습니까?',
+    );
+    result && history.goBack();
+  };
+
   return (
     <IonPage>
       <IonHeader translucent>
-        <BackButton title="Write" />
+        <IonToolbar>
+          <IonTitle size="large">
+            <div className="flex-row space-between">
+              Write
+              <IonButton slot="end" fill="clear" color="tertiary" onClick={onClickCancel}>
+                취소
+              </IonButton>
+            </div>
+          </IonTitle>
+        </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
