58b555a664cefa2689e0d038d571f66956ba9065
DEVFATH <40345474+dev-fath@users.noreply.github.com>
2023-02-08 18:06:30 +0900

    Merge pull request #12
    * [목록] 피드목록에 타임스탬프 컨버터 적용
    
    * [지도] 다크테마 적용

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,26 +1,37 @@
 import { IonPage } from '@ionic/react';
-import { useEffect } from 'react';
+import { useMemo } from 'react';
 
 import './Map.scss';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import BackButton from 'components/BackButton';
 
 const Map = () => {
-  useEffect(() => {
-    return () => {};
+  const prefersDark = useMemo(() => {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: Dark)').matches;
   }, []);
+
   return (
     <IonPage>
       <BackButton defaultHref="/home" />
       {window.google === undefined ? (
         <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY ?? ''}>
-          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={15}
+            options={{ styles: prefersDark ? darkModeStyle : null }}
+          >
             {/* Child components, such as markers, info windows, etc. */}
             <div>지도화면</div>
           </GoogleMap>
         </LoadScript>
       ) : (
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={15}
+          options={{ styles: prefersDark ? darkModeStyle : null }}
+        >
           {/* Child components, such as markers, info windows, etc. */}
           <div>지도화면</div>
         </GoogleMap>
@@ -28,6 +39,87 @@ const Map = () => {
     </IonPage>
   );
 };
+
+const darkModeStyle = [
+  { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+  { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+  { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [{ color: '#263c3f' }],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#6b9a76' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{ color: '#38414e' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#212a37' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#9ca5b3' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{ color: '#746855' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#1f2835' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#f3d19c' }],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [{ color: '#2f3948' }],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [{ color: '#17263c' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#515c6d' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [{ color: '#17263c' }],
+  },
+];
 
 const containerStyle = {
   width: '100%',
