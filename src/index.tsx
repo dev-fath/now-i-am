5dde7c9d236fe15e2aa38e7d9f9932e35b0596ae
DEVFATH <ggje33@gmail.com>
2023-02-10 14:22:40 +0900

    [리렌더링] 리렌더링 이슈 수정, StrictMode 제거

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root') as Element;
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
